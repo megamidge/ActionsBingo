b35f7f02936e39c21aac94c218969878befc301a
Sam <samsmith@leisure-electronics.co.uk>
2019-03-14 18:30:33 +0000

    Base mode done.

---
+++ ActionBingo/ActionBingo/Card.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionBingo
+{
+    public class Card
+    {
+        public string name;
+        public int[,] grid;
+        private int rows;
+        private int cols;
+        public Card(string name, int[,] grid, int rows, int cols)
+        {
+            this.name = name;
+            this.grid = new int[rows, cols];
+            for (int x = 0; x < cols; x++)
+                for (int y = 0; y < rows; y++)
+                    this.grid[x, y] = grid[x, y];
+
+            this.rows = rows;
+            this.cols = cols;
+        }
+        public override string ToString()
+        {
+            string ret = $"{name},";
+            for (int x = 0; x < cols; x++)
+                for (int y = 0; y < rows; y++)
+                    ret += $"{grid[x, y]},";
+            return ret;
+        }
+    }
+    class CardTest
+    {
+        public Base101 id;
+        public int[,] grid;
+        public CardTest(int[,] grid)
+        {
+
+        }
+    }
+
+    class Base101
+    {
+        private static Dictionary<int, char> base101Symbols = new Dictionary<int, char>()
+        {
+            [0] = '0',
+            [1] = '1',
+            [2] = '2',
+            [3] = '3',
+            [4] = '4',
+            [5] = '5',
+            [6] = '6',
+            [7] = '7',
+            [8] = '8',
+            [9] = '9',
+            [10] = 'A',
+            [11] = 'B',
+            [12] = 'C',
+[13] = 'D',
+[14] = 'E',
+[15] = 'F',
+[16] = 'G',
+[17] = 'H',
+[18] = 'I',
+[19] = 'J',
+[20] = 'K',
+[21] = 'L',
+[22] = 'M',
+[23] = 'N',
+[24] = 'O',
+[25] = 'P',
+[26] = 'Q',
+[27] = 'R',
+[28] = 'S',
+[29] = 'T',
+[30] = 'U',
+[31] = 'V',
+[32] = 'W',
+[33] = 'X',
+[34] = 'Y',
+[35] = 'Z',
+[36] = 'a',
+[37] = 'b',
+[38] = 'c',
+[39] = 'd',
+[40] = 'e',
+[41] = 'f',
+[42] = 'g',
+[43] = 'h',
+[44] = 'j',
+[45] = 'i',
+[46] = 'k',
+[47] = 'l',
+[48] = 'm',
+[49] = 'n',
+[50] = 'o',
+[51] = 'p',
+[52] = 'q',
+[53] = 'r',
+[54] = 's',
+[55] = 't',
+[56] = 'u',
+[57] = 'v',
+[58] = 'w',
+[59] = 'x',
+[60] = 'y',
+[61] = 'z',
+[62] = 'Ç',
+[63] = 'ü',
+[64] = 'é',
+[65] = 'â',
+[66] = 'ä',
+[67] = 'à',
+[68] = 'å',
+[69] = 'ç',
+[70] = 'ê',
+[71] = 'ë',
+[72] = 'è',
+[73] = 'ï',
+[74] = 'î',
+[75] = 'ì',
+[76] = 'Ä',
+[77] = 'Å',
+[78] = 'É',
+[79] = 'æ',
+[80] = 'Æ',
+[81] = 'ô',
+[82] = 'ö',
+[83] ='ò',
+[84]='û',
+[85]='ù',
+[86]='ÿ',
+[87]='Ö',
+[88]='Ü',
+[89]='¢',
+[90]='¥',
+[91]='₧',
+[92]='ƒ',
+[93]='á',
+[94]='í',
+[95]='ó',
+[96]='ú',
+[97]='ñ',
+[98]='Ñ',
+[99]='ª',
+[100]='º',
+        };
+        public static Base101 NumsToBase101(int[] grid)
+        {
+            int cells = grid.Length;
+            List<char> chars = new List<char>();
+            for(int i = cells-1; i >= 0; i--)
+            {
+                chars.Add(base101Symbols[grid[i]]);
+            }
+            Console.WriteLine(string.Join("", chars));
+            return null;
+        }
+    }
+}
--- ActionBingo/ActionBingo/Program.cs
+++ ActionBingo/ActionBingo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,77 +13,57 @@ namespace ActionBingo
         {
             int cols = 5;
             int rows = 5;
-            int[,] card = new int[cols, rows];
-            int[] cardFlat = new int[cols * rows];
-            Dictionary<int, string> actions = new Dictionary<int, string>()
+            Dictionary<int, string> actions = new Dictionary<int, string>();
+            List<Card> cards = new List<Card>();
+            string[] names = File.ReadAllLines("Data/F1BingoNames.csv");
+
+            string[] lines = File.ReadAllLines("Data/F1 Bingo - F1 2019.csv");
+            foreach (string line in lines)
             {
-                [0]="Hello",
-                [1]="World",
-                [2]="What",
-                [3]="When",
-                [4]="Who",
-                [5]="How",
-                [6]="Seven",
-                [7]="Formula 1",
-                [8]="Desktop",
-                [9]="Weirdos",
-                [10]="Kudos",
-                [11]="Japan",
-                [12]="Bingo",
-                [13]="EERC",
-                [14]="Taiwan",
-                [15]="Russia",
-                [16]="Syria",
-                [17]="Yeet",
-                [18]="Yote",
-                [19]="Scrote",
-                [20]="Yoda",
-                [21]="Han",
-                [22]="Hansje",
-                [23]="Hansgay",
-                [24]="W I L L",
-                [25]="F U C K",
-                [26]="T H E",
-                [27]="S I S T E R",
-                [28]="app.config",
-                [29]="Elementary",
-                [30]="Watson",
-                [31]="Renault",
-                [32]="Kubica",
-                [33]="Stroll",
-                [34]="Money",
-                [35]="Daddy",
-            };
+                string[] split = line.Split(',');
+                if (int.TryParse(split[0], out int res))
+                {
+                    actions.Add(res, split[1]);
+                }
+            }
             Random rng = new Random();
-            for(int i = 0; i < rows * cols; i++)
+            foreach(string name in names)
             {
-                int num = (int)rng.Next(actions.Keys.Min(), actions.Keys.Max() - 1);
-                while (cardFlat.Contains(num))
+                int[,] card = new int[cols, rows];
+                int[] cardFlat = new int[cols * rows];
+                for(int i = 0; i < rows * cols; i++)
                 {
-                    num = (int)rng.Next(actions.Keys.Min(), actions.Keys.Max() - 1);
+                    int num = (int)rng.Next(actions.Keys.Min(), actions.Keys.Max() - 1);
+                    while (cardFlat.Contains(num))
+                    {
+                        num = (int)rng.Next(actions.Keys.Min(), actions.Keys.Max() - 1);
+                    }
+                    cardFlat[i] = num;
                 }
-                cardFlat[i] = num;
-            }
+                for(int i = 0; i < 5; i++)
+                {
+                    int randIndex = rng.Next(0, cols * rows);
+                    while(cardFlat[randIndex] == -1)
+                    {
+                        randIndex = rng.Next(0, cols * rows);
+                    }
+                    cardFlat[randIndex] = -1;
+                }
+                for(int x = 0; x < cols; x++)
+                {
+                    for(int y = 0; y < rows; y++)
+                    {
+                        card[x, y] = cardFlat[cols * x + y];
+                    }
+                }
 
-            for(int x = 0; x < cols; x++)
+                cards.Add(new Card(name, card, rows, cols));
+            }
+            using (StreamWriter sw = new StreamWriter(File.Open("cards.csv", FileMode.Create)))
             {
-                for(int y = 0; y < rows; y++)
-                {
-                    card[x, y] = cardFlat[cols * x + y];
-                }
+                foreach (Card card in cards)
+                    sw.WriteLine(card.ToString());
             }
-
-            for(int x = 0; x < cols; x++)
-            {
-                for(int y = 0; y < rows; y++)
-                {
-                    Console.Write($"{actions[card[x,y]]}\t\t\t");
-                }
-                Console.WriteLine();
-                Console.WriteLine();
-                Console.WriteLine();
-            }
-
             Console.ReadLine();
         }
     }
